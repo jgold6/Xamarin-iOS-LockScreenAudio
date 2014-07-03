a4a9f11ef4542544748f277199e24f7a6359fe96
Jon Goldberger <>
2014-07-02 22:39:42 -1000

    Added search functionality. Searches on artist and song
    * ArtistSongTableViewSource.cs: Separated the Source from the
      UITableViewController so the search controller could use it too. 
    
    * LockScreenAudio.csproj: 
    
    * MasterViewController.cs: Added search bar and search weak delegate.
    
    * Songs.cs: Songs is now responsible for testing if the user is
      searching and returning the appropriate results. .

--- LockScreenAudio/MasterViewController.cs
+++ LockScreenAudio/MasterViewController.cs
@@ -15,15 +15,11 @@ using MonoTouch.ObjCRuntime;
 
 namespace LockScreenAudio
 {
-	public enum PlayerOption
-	{
-		Stream = 0,
-		StreamAndSave
-	}
-
 	public partial class MasterViewController : UITableViewController
 	{
-		UIBarButtonItem leftBBI;
+		private UISearchBar searchBar;
+		private UISearchDisplayController searchController;
+		private UIBarButtonItem leftBBI;
 
 		#region - Constructors
 		public MasterViewController(IntPtr handle) : base(handle)
@@ -49,6 +45,22 @@ namespace LockScreenAudio
 			Songs.querySongs();
 			this.Title = String.Format("Songs ({0}) by Artist ({1})", Songs.songCount, Songs.artistCount);
 
+			tableView.Source = new ArtistSongTableViewSource(this);
+
+			searchBar = new UISearchBar(new RectangleF (0, 0, 320, 44)) {
+				Placeholder = "Search",
+				AutocorrectionType = UITextAutocorrectionType.No,
+				AutocapitalizationType = UITextAutocapitalizationType.None
+			};
+			searchBar.SizeToFit();
+
+			TableView.TableHeaderView = searchBar;
+
+			searchController = new UISearchDisplayController(searchBar, this);
+
+			searchController.WeakDelegate = this;
+			searchController.SearchResultsSource = tableView.Source;
+
 			leftBBI = new UIBarButtonItem("Stream a song", UIBarButtonItemStyle.Bordered, this, new Selector("StreamSong:"));
 			this.NavigationItem.LeftBarButtonItem = leftBBI;
 			this.TableView.SectionIndexTrackingBackgroundColor = UIColor.FromRGB(0.9f, 0.9f, 0.9f);
@@ -72,9 +84,17 @@ namespace LockScreenAudio
 					detailVC.song = song;
 
 				}
-				else {
+				else if (!Songs.searching) {
 					// Selected song
 					NSIndexPath indexPath = this.TableView.IndexPathForSelectedRow;
+					Song song = Songs.GetSongBySectionRow(indexPath.Section, indexPath.Row);
+					song.streamingURL = null;
+					// Pass song info to the detail view controller
+					detailVC.song = song;
+				}
+				else {
+					UITableView tv = sender as UITableView;
+					NSIndexPath indexPath = tv.IndexPathForSelectedRow;
 					Song song = Songs.GetSongBySectionRow(indexPath.Section, indexPath.Row);
 					song.streamingURL = null;
 					// Pass song info to the detail view controller
@@ -90,131 +110,6 @@ namespace LockScreenAudio
 
 		#endregion
 
-		#region - Table View data source overrides
-		public override int NumberOfSections(UITableView tableView)
-		{
-			return Songs.artistCount;
-		}
-
-		public override int RowsInSection(UITableView tableview, int section)
-		{
-			return Songs.GetSongsByArtistIndex(section).Count;
-		}
-
-		string[] alphabet = new string[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-		string[] numbers = new string[]{"0","1","2","3","4","5","6","7","8","9"};
-
-		//Provide an index in the table for quick scrolling
-		public override string[] SectionIndexTitles(UITableView tableView)
-		{
-			List<string> index = new List<string>();
-			List<string> artists = Songs.GetListOfArtists();
-			string lastChar = "";
-
-			foreach (string artist in artists) {
-				string initialLetter = "";
-				if (artist.Length > 4 && artist.Substring(0,4) == "The ") {
-					initialLetter = artist.Substring(4,1).ToUpper();
-				}
-				else if (artist.Length > 2 && artist.Substring(0,2) == "A ") {
-					initialLetter = artist.Substring(2,1).ToUpper();
-				}
-				else if (artist.Length >1)
-					initialLetter = artist.Substring(0,1).ToUpper();
-
-				// Handle numbers first
-				if (artist.Length > 1 && numbers.Contains<string>(initialLetter) && !index.Contains("#")) {
-					index.Add("#");
-					continue;
-				}
-				// Ignore artists that start with non-alphabetic characters
-				if (artist.Length > 1 && !alphabet.Contains<string>(initialLetter)) {
-					continue;
-				}
-				// Add new initial letter to List
-				if (initialLetter != lastChar) {
-					lastChar = initialLetter;
-					index.Add(initialLetter);
-				}
-			}
-			return index.ToArray();
-		}
-
-		// Get the section to scroll to when the side index is used
-		public override int SectionFor(UITableView tableView, string title, int atIndex)
-		{
-			List<string> artists = Songs.GetListOfArtists();;
-			int section = 0;
-
-			foreach (string artist in artists) {
-				string initialLetter = "";
-				if (artist.Length > 4 && artist.Substring(0,4) == "The ") {
-					initialLetter = artist.Substring(4,1).ToUpper();
-				}
-				else if (artist.Length > 2 && artist.Substring(0,2) == "A ") {
-					initialLetter = artist.Substring(2,1).ToUpper();
-				}
-				else if (artist.Length >1)
-					initialLetter = artist.Substring(0,1).ToUpper();
-
-				// Ignore non-alphanumeric characters
-				if (!alphabet.Contains<string>(initialLetter) && !numbers.Contains<string>(initialLetter)) {
-					section++;
-					continue;
-				}
-				// Is this the first artist with the initial letter equal to index letter (title parameter) or a number? 
-				if (initialLetter == title || numbers.Contains<string>(initialLetter))
-					return section;
-				else
-					section++;
-			}
-			return 0;
-		}
-
-		// Get the view for the section header - create and style container view, label, and image view
-		public override UIView GetViewForHeader(UITableView tableView, int section)
-		{
-			UILabel headerLabel = new UILabel();
-			headerLabel.Frame = new RectangleF(40.0f, 5.0f, tableView.Frame.Width -60.0f, 20.0f);
-			headerLabel.Font = UIFont.PreferredHeadline;
-			headerLabel.Text = TitleForHeader(tableView, section);
-			headerLabel.TextColor = UIColor.White;
-
-			UIImageView artworkView = new UIImageView(new RectangleF(0.0f, 0.0f, 30.0f, 30.0f));
-			NSIndexPath indexPath = NSIndexPath.FromRowSection(0, section);
-			Song song = Songs.GetSongBySectionRow(indexPath.Section, indexPath.Row);
-			artworkView.Image = song.artwork.ImageWithSize(new SizeF(30.0f, 30.0f));
-
-			UIView headerView = new UIView();
-			headerView.BackgroundColor = UIColor.DarkGray;
-			headerView.Add(headerLabel);
-			headerView.Add(artworkView);
-
-			return headerView;
-		}
-
-		// Get the text for the section header view 
-		public override string TitleForHeader(UITableView tableView, int section)
-		{
-			return Songs.GetArtistByIndex(section);
-		}
-
-		// Set up the table view cells
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
-			UITableViewCell cell = tableView.DequeueReusableCell("cell");
-			if (cell == null)
-				cell = new UITableViewCell(UITableViewCellStyle.Subtitle, "cell");
-
-			Song song = Songs.GetSongBySectionRow(indexPath.Section, indexPath.Row);
-
-			cell.TextLabel.Text = song.song;
-			cell.DetailTextLabel.Text = String.Format("Album: {0}", song.album);
-
-			return cell;
-		}
-		#endregion
-
 		#region - class helper methods
 		[Export("StreamSong:")]
 		public void StreamSong(UIBarButtonItem sender)
@@ -222,6 +117,37 @@ namespace LockScreenAudio
 			PerformSegue("showDetail", sender);
 		}
 
+		#endregion
+
+		#region - Search WeakDelegate
+		[Export ("searchDisplayControllerWillBeginSearch:")]
+		public void WillBeginSearch(UISearchDisplayController controller)
+		{
+			Songs.FilterContentsForSearch("");
+			Songs.searching = true;
+//			tableView.ReloadData();
+//			controller.SearchResultsTableView.ReloadData();
+		}
+
+		[Export ("searchDisplayController:shouldReloadTableForSearchString:")]
+		public bool ShouldReloadForSearchString(UISearchDisplayController controller, string forSearchString)
+		{
+//			var source = controller.SearchResultsTableView.Source as ArtistSongTableViewSource;
+			Songs.FilterContentsForSearch(forSearchString);
+//			tableView.ReloadData();
+//			controller.SearchResultsTableView.ReloadData();
+			//ControllerHelpers.TitleCountUpdater(source.GetItemCount());
+			return true;
+		}
+
+		[Export ("searchDisplayControllerDidEndSearch:")]
+		public void DidEndSearch(UISearchDisplayController controller)
+		{
+			Songs.searching = false;
+//			tableView.ReloadData();
+//			controller.SearchResultsTableView.ReloadData();
+			//ControllerHelpers.TitleCountUpdater(source.GetItemCount());
+		}
 		#endregion
 	}
 }
--- LockScreenAudio/Songs.cs
+++ LockScreenAudio/Songs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 namespace LockScreenAudio
 {
@@ -9,6 +10,13 @@ namespace LockScreenAudio
 		public static Dictionary<string, List<Song>> artistSongs = new Dictionary<string, List<Song>>();
 		public static int songCount { get; private set;}
 		public static int artistCount { get; private set;}
+
+
+		public static Dictionary<string, List<Song>> searchResults;
+		public static int searchSongCount { get; private set;}
+		public static int searchArtistCount { get; private set;}
+
+		public static bool searching = false;
 
 		// Get the songs from the music library
 		public static void querySongs()
@@ -22,11 +30,17 @@ namespace LockScreenAudio
 			foreach (List<Song> songs in artists) {
 				songCount += songs.Count;
 			}
+
+			searchSongCount = 0;
+			searchArtistCount = 0;
 		}
 
 		public static List<string> GetListOfArtists()
 		{
-			return artistSongs.Keys.ToList();
+			if (searching)
+				return searchResults.Keys.ToList();
+			else
+				return artistSongs.Keys.ToList();
 		}
 
 		public static List<Song> GetSongsByArtist(string artistName)
@@ -38,14 +52,26 @@ namespace LockScreenAudio
 
 		public static List<Song> GetSongsByArtistIndex(int index)
 		{
-			var artistSongList = artistSongs.Values.ToList();
-			return artistSongList[index];
+			if (searching) {
+				var searchSongList = searchResults.Values.ToList();
+				return searchSongList[index];
+			}
+			else {
+				var artistSongList = artistSongs.Values.ToList();
+				return artistSongList[index];
+			}
 		}
 
 		public static Song GetSongBySectionRow(int section, int row)
 		{
-			var artistSongList = artistSongs.Values.ToList();
-			return artistSongList[section][row];
+			List<List<Song>> SongList;
+			if (searching)
+				SongList = searchResults.Values.ToList();
+			else
+				SongList = artistSongs.Values.ToList();
+			return SongList[section][row];
+//			var artistSongList = artistSongs.Values.ToList();
+//			return artistSongList[section][row];
 		}
 
 		public static int GetIndexOfArtist(string artistName)
@@ -63,7 +89,10 @@ namespace LockScreenAudio
 
 		public static string GetArtistByIndex(int index)
 		{
-			return artistSongs.Keys.ToList()[index];
+			if (searching)
+				return searchResults.Keys.ToList()[index];
+			else
+				return artistSongs.Keys.ToList()[index];
 		}
 
 		public static Song GetSongByTitle(string title)
@@ -88,6 +117,24 @@ namespace LockScreenAudio
 					index++;
 			}
 			return 0;
+		}
+
+		public static void FilterContentsForSearch(string str)
+		{
+			searchResults = artistSongs.Where(a => a.Key.ToLower().Contains(str.ToLower())).ToDictionary(a => a.Key, a => a.Value);
+			Dictionary<string, List<Song>> otherArtistSongs = artistSongs.Where(a => !a.Key.ToLower().Contains(str.ToLower())).ToDictionary(a => a.Key, a => a.Value);
+			foreach (List<Song> songs in otherArtistSongs.Values) {
+				List<Song> matchingSongs = songs.Where(a => a.song.ToLower().Contains(str.ToLower())).ToList();
+				if (matchingSongs.Count > 0)
+					searchResults.Add(matchingSongs[0].artist, matchingSongs);
+			}
+			searchArtistCount = searchResults.Keys.Count;
+			var artists = searchResults.Values;
+			searchSongCount = 0;
+			foreach (List<Song> songs in artists) {
+				searchSongCount += songs.Count;
+			}
+			searchResults = searchResults.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
 		}
 	}
 }
