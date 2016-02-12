5d8e58e0d75f0ffa313daa8ae254afffe9c81e65
Jon Goldberger <jon.goldberger@xamarin.com>
2016-02-11 22:07:25 -1000

    test3

--- LockScreenAudio/MasterViewController.cs
+++ LockScreenAudio/MasterViewController.cs
@@ -1,7 +1,6 @@
 ﻿// Loosely based on this guide: http://www.sagorin.org/ios-playing-audio-in-background-audio/
 // and sample in Obj-C: https://github.com/jsagorin/iOSBackgroundAudio
 
-
 using System;
 using CoreGraphics;
 using System.Collections.Generic;
